f347b08e996af230979196df85a109ceb40ccaae
Tanvir Ahmed (Biplob) <75024294+codingzonehere@users.noreply.github.com>
2025-04-27 22:17:53 +0600

    codingzonehere

---
+++ apps.js
@@ -0,0 +1,7 @@
+function ignoreCall() {
+    document.getElementById("popup").style.display = "none"; // Hide popup
+  }
+
+  function allowCall() {
+    document.getElementById("callLink").click(); // Trigger call
+  }
